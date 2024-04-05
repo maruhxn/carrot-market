852c93a6e97b396d4f288c327756b373e4606900
maruhxn <maruhan1016@gmail.com>
2024-04-05 23:24:11 +0900

    Server Actions

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,12 @@
-"use client";
-
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
 export default function Login() {
-  const onClick = async () => {
-    const response = await fetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify({
-        username: "nico",
-        password: "1234",
-      }),
-    });
-    console.log(await response.json());
+  const handleForm = async (formData: FormData) => {
+    "use server";
+    console.log(formData.get("email"), formData.get("password"));
+    console.log("i run in teh server baby!");
   };
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
@@ -21,18 +14,23 @@ export default function Login() {
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password!</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="Email" required errors={[]} />
+      <form action={handleForm} className="flex flex-col gap-3">
         <FormInput
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          errors={[]}
+        />
+        <FormInput
+          name="password"
           type="password"
           placeholder="Password"
           required
           errors={[]}
         />
-      </form>
-      <span onClick={onClick}>
         <FormButton text="Log in" isLoading={false} />
-      </span>
+      </form>
       <SocialLogin />
     </div>
   );
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -10,12 +10,14 @@ export default function SMSLogin() {
       </div>
       <form className="flex flex-col gap-3">
         <FormInput
+          name="phoneNumber"
           type="number"
           placeholder="Phone number"
           required
           errors={[]}
         />
         <FormInput
+          name="verificationCode"
           type="number"
           placeholder="Verification code"
           required
