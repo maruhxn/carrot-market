e614be2b548b1dc51eff32d6fab46c1d4df22906
maruhxn <maruhan1016@gmail.com>
2024-04-08 23:54:27 +0900

    Refactor

--- components/button.tsx
+++ components/button.tsx
@@ -2,11 +2,11 @@
 
 import { useFormStatus } from "react-dom";
 
-interface FormButtonProps {
+interface ButtonProps {
   text: string;
 }
 
-export default function FormButton({ text }: FormButtonProps) {
+export default function FormButton({ text }: ButtonProps) {
   const { pending } = useFormStatus(); // 이 hook은 form의 자식 요소에서만 사용되어야 한다. => form의 상태에 따라 변경하고자 하는 component 내부에서 사용해야 한다. => FormButton에서 사용하자.
   // 자동으로 부모 form을 찾고 상호작용할 것.
   return (
