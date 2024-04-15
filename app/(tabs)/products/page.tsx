f30e64f789afafea3702700f69a60e3a740e4951
maruhxn <maruhan1016@gmail.com>
2024-04-15 10:55:48 +0900

    Product Upload Page & Action

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,7 +1,9 @@
 import ProductList from "@/components/product-list";
 import { PRODUCTS_PAGE_SIZE } from "@/lib/constants";
 import db from "@/lib/db";
+import { PlusIcon } from "@heroicons/react/24/solid";
 import { Prisma } from "@prisma/client";
+import Link from "next/link";
 
 async function getInitialProducts() {
   const products = await db.product.findMany({
@@ -27,5 +29,16 @@ export type InitialProducts = Prisma.PromiseReturnType<
 
 export default async function Products() {
   const initialProducts = await getInitialProducts();
-  return <ProductList initialProducts={initialProducts} />;
+  return (
+    <div>
+      <ProductList initialProducts={initialProducts} />
+      <Link
+        href="/products/add"
+        className="bg-orange-500 flex items-center justify-center rounded-full size-16 fixed
+      bottom-24 right-8 text-white transition-colors hover:bg-orange-400"
+      >
+        <PlusIcon className="size-10" />
+      </Link>
+    </div>
+  );
 }
